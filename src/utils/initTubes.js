6103f3c4b7eeafdf2a00b5842fd4042fcd55eb98
Lnola <nola.luka@gmail.com>
2022-09-20 00:00:24 +0200

    Add number of tubes and default values

--- src/utils/initTubes.js
+++ src/utils/initTubes.js
@@ -1,19 +1,20 @@
 import shuffle from 'shuffle-array';
 import colors from 'constants/colors';
 
-const initTubes = (numberOfTubes) => {
-  const randomColors = shuffle(colors).slice(0, numberOfTubes);
+const initTubes = (numberOfFilledTubes = 4, numberOfEmptyTubes = 2) => {
+  const numberOfTubes = numberOfFilledTubes + numberOfEmptyTubes;
+  const randomColors = shuffle(colors).slice(0, numberOfFilledTubes);
   let sameColorTubes = [];
   let tubes = [];
 
-  for (let i = numberOfTubes; i > 0; i--)
+  for (let i = numberOfFilledTubes; i > 0; i--)
     sameColorTubes = sameColorTubes.concat(
       new Array(4).fill(randomColors[i - 1])
     );
 
   sameColorTubes = shuffle(sameColorTubes);
 
-  for (let i = 0; i < numberOfTubes + 2; i++)
+  for (let i = 0; i < numberOfTubes; i++)
     tubes.push(sameColorTubes.splice(0, 4));
 
   return tubes;
