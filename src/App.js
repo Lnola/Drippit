71f205cf44502d60d0eee84decb44afc9c6f4817
Lnola <nola.luka@gmail.com>
2022-09-19 23:54:51 +0200

    Move root to global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import GlobalStyles from 'index.styled';
 import React from 'react';
 import Game from 'screens';
-import 'styles.css';
 
 const App = () => {
-  return <Game />;
+  return (
+    <>
+      <GlobalStyles />
+      <Game />
+    </>
+  );
 };
 
 export default App;
---
+++ src/index.styled.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  #root {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export default GlobalStyles;
